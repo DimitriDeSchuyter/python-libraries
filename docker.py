37dc195e17d7f1fb7ef5f1b4b1d58ac3f7f70b57
Frederic Depuydt <f.depuydt@outlook.com>
2020-05-19 09:50:34 +0100

    first commit

---
+++ docker.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+############################################################################
+## LIBRARY for DOCKER Handling                                            ##
+## Author: Frederic Depuydt                                               ##
+## Mail: frederic.depuydt@outlook.com                                     ##
+############################################################################
+
+from echo import echo;
+from command import command;
+import os, sys;
+
+class docker:
+
+    @staticmethod # DOCKER EXEC
+    def exec(flags, container, cmd):
+        try:
+            docker.container.exists(container);
+        except dockerError as e:
+            echo.error(str(e));
+            sys.exit(1);
+        cmd = "docker exec " + flags + " " + container + " bash -c \"" + cmd.replace("\\","\\\\").replace("\"","\\\"") + "\""; 
+        command.exec(cmd);
+
+    @staticmethod # DOCKER CP
+    def cp(source, destination):
+        cmd = "docker cp " + source + " " + destination;
+        command.exec(cmd);
+
+
+    class container:
+        @staticmethod # CONTAINER EXISTS
+        def exists(container):
+            cmd = "docker ps -q -f name=\"" + container.replace("\"", "\\\"") + "\"";
+            id = len(list(filter(None,command.exec(cmd, False).split("\n"))));
+            if id == 0:
+                raise dockerError("Container `" + container + "` doesn't exist");
+            elif id > 1:
+                raise dockerError("Multiple containers matched `" + container + "`");
+
+    class volume:
+        @staticmethod # VOLUME EXISTS
+        def exists(volume):
+            cmd = "docker volume ls -q -f name=\"" + volume.replace("\"", "\\\"") + "\"";
+            id = len(list(filter(None,command.exec(cmd, False).split("\n"))));
+            if id == 0:
+                raise dockerError("Volume `" + volume + "` doesn't exist");
+            elif id > 1:
+                raise dockerError("Multiple volumes matched `" + volume + "`");
+
+    class network:
+        @staticmethod # NETWORK EXISTS
+        def exists(network):
+            cmd = "docker network ls -q -f name=\"" + network.replace("\"", "\\\"") + "\"";
+            id = len(list(filter(None,command.exec(cmd, False).split("\n"))));
+            if id == 0:
+                raise dockerError("Network `" + network + "` doesn't exist");
+            elif id > 1:
+                raise dockerError("Multiple networks matched `" + network + "`");
+
+
+    class compose:
+        @staticmethod # DOCKER COMPOSE UP
+        def up(flags = None, extra = None):
+            if flags == None:
+                flags = "";
+            if extra == None:
+                extra = "";
+            command.exec("docker-compose up " + flags + " " + extra);
+
+        @staticmethod # DOCKER COMPOSE RUN
+        def run(flags, container, cmd = None):
+            if cmd == None:
+                cmd = "";
+            command.exec("docker-compose run " + flags + " " + container + " " + cmd);
+
+        @staticmethod # DOCKER COMPOSE DOWN
+        def down(flags = None, extra = None):
+            if flags == None:
+                flags = "";
+            if extra == None:
+                extra = "";
+            command.exec("docker-compose down " + flags + " " + extra);
+
+        @staticmethod # DOCKER COMPOSE STOP
+        def stop(flags = None, extra = None):
+            if flags == None:
+                flags = "";
+            if extra == None:
+                extra = "";
+            command.exec("docker-compose stop " + flags + " " + extra);
+
+class dockerError(Exception):
+    pass
+
+
+if __debug__:
+    echo.debug("imported `docker.py`");
